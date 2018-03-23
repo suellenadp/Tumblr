4b0409c81a5e300e9b750616521e9e60267a392f
Suellen Aline de Paula <suellen.alinep@gmail.com>
2018-03-23 15:42:30 -0300

    tumblr

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
- resources :posts
+  devise_for :users
+ resources :posts do
+ 	resources :comments
+ end	
  root "posts#index"
+ 
+ get '/about', to: 'pages#about' 
 end
  
